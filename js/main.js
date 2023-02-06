22c6bca2a42d1a3bbadb6c271889bccbd3566792
saymonks <kabannikov-k@mail.ru>
2023-02-06 21:12:21 +0600

    settings sliders

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
-const swiper = new Swiper('.hotel-slider', {
+const hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
   // Navigation arrows
   navigation: {
     nextEl: '.hotel-slider__button--next',
     prevEl: '.hotel-slider__button--prev',
+  },
+
+  keyboard: {
+    enabled: true,
+  },
+});
+
+const reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: 'swiper-button-prev',
+    prevEl: 'swiper-button-next',
   },
 
   keyboard: {
